0fa4f48252f12ebbe767a0a536d8fe9b9ad666e2
rsimmons11 <ptgrd1980@gmail.com>
2024-01-19 17:19:51 -0800

    added rules to eslintrc

--- components/contact.tsx
+++ components/contact.tsx
@@ -31,9 +31,9 @@ export default function Contact() {
     >
       <SectionHeading>Contact me</SectionHeading>
       <p className='text-gray-700 -mt-6 dark:text-white/80'>Please contact me directly at{' '} 
-        <a className='underline' href='mailto:example@gmail.com'
+        <a className='underline' href='mailto:ptgrd1980@gmail.com'
         >
-          example@gmail.com
+          ptgrd1980@gmail.com
         </a>{' '} 
         or through this form.
       </p>
--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  experimental:{
-     serverActions:true, 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
